08cd913dbdce47e22fca7ea2e3220d8b7a9ce290
od1e0 <88845348+od1e0@users.noreply.github.com>
2022-09-21 22:44:58 +0300

    first commit

---
+++ Assets/Scripts/Lessons.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Lessons : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private SpriteRenderer _background;
+    
+    [SerializeField]
+    private CharacterView _characterView;
+
+    [SerializeField]
+    private SpriteAnimationConfig _spriteAnimationConfig;
+    
+    [SerializeField]
+    private CannonView _cannonView;
+
+    [SerializeField] 
+    private List<BulletView> _bullets;
+
+    private ParalaxManager _paralaxManager;
+    private SpriteAnimator _spriteAnimator;
+    private MainHeroWalker _mainHeroWalker;
+    private AimingMuzzle _aimingMuzzle;
+    private BulletsEmitter _bulletsEmitter;
+
+    private void Start()
+    {
+        _paralaxManager = new ParalaxManager(_camera, _background.transform);
+        _spriteAnimator = new SpriteAnimator(_spriteAnimationConfig);
+        _mainHeroWalker = new MainHeroWalker(_characterView, _spriteAnimator);
+        _aimingMuzzle = new AimingMuzzle(_cannonView.transform, _characterView.transform);
+        _bulletsEmitter = new BulletsEmitter(_bullets, _cannonView.MuzzleTransform);
+    }
+
+    private void Update()
+    {
+        _paralaxManager.Update();
+        _spriteAnimator.Update();
+        _mainHeroWalker.Update();
+        _aimingMuzzle.Update();
+        _bulletsEmitter.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ParalaxManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ParalaxManager
+{
+    private Camera _camera;
+    private Transform _backTransform;
+    private Vector3 _backStartPosition;
+    private Vector3 _cameraStartPosition;
+
+    private const float Coef = 1f;
+
+    public ParalaxManager(Camera camera, Transform backTransform)
+    {
+        _camera = camera;
+        _backTransform = backTransform;
+        _backStartPosition = backTransform.position;
+        _cameraStartPosition = camera.transform.position;
+    }
+
+    public void Update()
+    {
+        _backTransform.position = _backStartPosition + (_camera.transform.position - _cameraStartPosition) * Coef;
+    }
+}
