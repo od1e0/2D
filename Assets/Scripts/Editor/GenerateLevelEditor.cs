c870ee1b3087bcd921d175293c737760403091dc
od1e0 <88845348+od1e0@users.noreply.github.com>
2022-10-06 23:29:23 +0300

    6Dz

---
+++ Assets/Scripts/Editor/GenerateLevelEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GenerateLevelView))]
+public class GenerateLevelEditor : Editor
+{
+    private GenerateLevelController _generateLevelController;
+
+    private void OnEnable()
+    {
+        var generateLevelView = (GenerateLevelView)target;
+        _generateLevelController = new GenerateLevelController(generateLevelView);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        var tileMap = serializedObject.FindProperty("_tileMapGround");
+        EditorGUILayout.PropertyField(tileMap);
+
+        if (GUI.Button(new Rect(10, 0, 60, 50), "Generate"))
+            _generateLevelController.Awake();
+
+        if (GUI.Button(new Rect(10, 55, 60, 50), "Clear"))
+            _generateLevelController.ClearTileMap();
+
+        GUILayout.Space(100);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
